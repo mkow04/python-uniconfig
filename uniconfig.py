1fb0381460a8ba05343284830c133b563b373ec1
mkow04 <maciejkowalski04@proton.me>
2025-03-07 02:27:56 +0100

    Fixed bugs, added early dictionary key check option on load

--- uniconfig.py
+++ uniconfig.py
@@ -31,6 +31,16 @@ class ConfigNotEditedException(BaseUniConfigException):
     pass
 
 
+class ConfigFreshlyCreatedException(ConfigNotEditedException):
+    """Exception inheriting from ConfigNotEditedException, thrown when config was freshly created from default_config while is_comparing_with_default == True"""
+    pass
+
+
+class ConfigKeysDontMatchException(BaseUniConfigException):
+    """Exception thrown when dictionary keys of the config don't match the keys of the default config"""
+    pass
+
+
 ################################################################
 # Config class
 
@@ -38,19 +48,27 @@ class Config:
     def __init__(self, config_file_path: str,
                  default_config: dict = None,
                  is_creating_new_config: bool = True,
-                 is_comparing_with_default: bool = True):
+                 is_comparing_with_default: bool = True,
+                 is_checking_keys_on_load: bool = True):
 
         self.is_creating_new_config = is_creating_new_config
         self.is_comparing_with_default = is_comparing_with_default
+        self.is_checking_keys_on_load = is_checking_keys_on_load
 
-        if self.default_config is not None:
+        if default_config is not None:
             self.default_config = default_config
         else:
             self.default_config = {}
 
-        self.config_file_path = Path(config_file_path)
+        self.config_file_path = Path(config_file_path).expanduser()
 
         self.load()
+
+    def check_keys(self):
+        if self.config.keys() == self.default_config.keys():
+            return True
+        else:
+            return False
 
     def create_default(self):
         self.config_file_path.parent.mkdir(parents=True, exist_ok=True)
@@ -59,21 +77,28 @@ class Config:
             toml.dump(self.default_config, config_file)
 
     def load(self):
+        was_default_created = False
+
         try:
             with self.config_file_path.open("r", errors="ignore") as config_file:
-                toml_config = toml.load(config_file)
+                self.config = toml.load(config_file)
         except FileNotFoundError as e:
-            self.config = {}
-
             if self.is_creating_new_config:
                 self.create_default()
+                self.config = self.default_config.copy()
+                was_default_created = True
             else:
                 raise e
 
-        if self.is_comparing_with_default and toml_config == self.default_config:
-            raise ConfigNotEditedException
+        if self.is_comparing_with_default and self.config == self.default_config:
+            if was_default_created:
+                raise ConfigFreshlyCreatedException
+            else:
+                raise ConfigNotEditedException
 
-        self.config = toml_config
+        if self.is_checking_keys_on_load:
+            if not self.check_keys():
+                raise ConfigKeysDontMatchException
 
 
 ################################################################
@@ -82,8 +107,8 @@ class Config:
 def main():
     print()
     print("-"*48)
-    print(f"{__package_name__.capitalize()} '{__version__}'")
-    print(f"Author: '{__author__} <{__email__}>'")
+    print(f" {__package_name__.capitalize()} '{__version__}'")
+    print(f" Author: '{__author__} <{__email__}>'")
     print("-"*48)
     print()
 
